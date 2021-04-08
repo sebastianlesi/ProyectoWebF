6360b5f14966e107996561f8617a28d7ea072486
sebastianlesi <sebastianlesi@unisabana.edu.co>
2021-04-08 17:12:33 +0200

    fgffg

--- eracom/settings.py
+++ eracom/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'era.apps.EraConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
