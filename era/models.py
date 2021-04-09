7baf3907e5a24a3f6ec3814f0fa93ce7383ef9db
sebastianlesi <sebastianlesi@unisabana.edu.co>
2021-04-09 16:36:08 +0200

    listo

--- era/models.py
+++ era/models.py
@@ -11,7 +11,7 @@ class Carrera(models.Model):
     name = models.CharField(max_length=45)
     abreviado = models.CharField(max_length=20)
     description = models.CharField(max_length=250)
-    Facultad_idFacultad = models.ForeignKey(Facultad.id_Facultad)
+    """Facultad_idFacultad = models.ForeignKey(Facultad.id_Facultad, on_delete= models.CASCADE)"""
 
 class Alumno(models.Model):
     id_alumno = models.AutoField(primary_key=True, unique=True, editable=False)
@@ -23,15 +23,15 @@ class Alumno(models.Model):
     clave = models.CharField(max_length=35)
     fecha_nac= models.DateTimeField()
     Genero = models.CharField(max_length=20)
-    Carrera_idCarrera = models.ForeignKey(Carrera.id_carrera)
-    Intercambio_idintercambio = models.ForeignKey(Intercambio.id_intercambio)
+    """Carrera_idCarrera = models.ForeignKey(Carrera.id_carrera)"""
+    """Intercambio_idintercambio = models.ForeignKey(Intercambio.id_intercambio)"""
 
 class Comentario(models.Model):
     id_comentario = models.AutoField(primary_key=True, unique=True, editable=False)
     Comentario = models.CharField(max_length=750)
     fecha= models.DateTimeField()
     hora = models.TimeField()
-    Alumno_idalumno = models.ForeignKey(Alumno.id_alumno)
+    """Alumno_idalumno = models.ForeignKey(Alumno.id_alumno)"""
 #--------------------------------------------
 class Publicacion(models.Model):
     id_publicacion = models.AutoField(primary_key=True, unique=True, editable=False)
@@ -40,13 +40,13 @@ class Publicacion(models.Model):
     fecha= models.DateTimeField()
     hora = models.TimeField()
     vistas = models.IntegerField()
-    Alumno_idAlumno = models.ForeignKey(Alumno.id_alumno)
+    """Alumno_idAlumno = models.ForeignKey(Alumno.id_alumno)"""
 
 class Multimedia(models.Model):
     id_multimedia = models.AutoField(primary_key=True, unique=True, editable=False)
     url = models.CharField(max_length=150)
     tipo= models.CharField(max_length=45)
-    Publicacion_idPublicacion = models.ForeignKey(Publicacion.id_publicacion)
+    """Publicacion_idPublicacion = models.ForeignKey(Publicacion.id_publicacion)"""
 
 #-----------------------------------------------
 class Intercambio(models.Model):
