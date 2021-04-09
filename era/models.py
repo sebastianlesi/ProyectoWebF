e378f3d15d59b183f45bb5f65c533d6ef0bbdcbd
Cholele <edder-hdez@hotmail.comn>
2021-04-09 18:20:38 +0200

    nnbnmn

--- era/models.py
+++ era/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Facultad(models.Model):
-    id_Facultad = models.AutoField(primary_key=True)
+    id_Facultad = models.AutoField(primary_key=True, unique=True, editable=False)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
 
